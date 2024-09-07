5f90cbf9146472789d586439dbf7c924936c84ed
Vinícius Henrique <164516075+Vinizin0509@users.noreply.github.com>
2024-09-06 21:03:44 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+let dados = [
+    {
+        titulo: "Rodrigo Garro",
+        descricao: "Rodrigo Garro é um meia argentino que conquistou o coração da Fiel Torcida Conhecido por sua habilidade e visão de jogo, o argentino se destaca pelo seu pé esquerdo preciso e por sua capacidade de criar jogadas perigosas.",
+        link: "https://pt.wikipedia.org/wiki/Rodrigo_Garro",
+        tags: "argentino meio campista craque camisa 10 canhoto baxinho "
+    },
+    {
+
+        titulo: "Yuri Alberto",
+        descricao: "Yuri Alberto é um atacante brasileiro conhecido por sua velocidade e finalização. Destacou-se em diversos clubes e atualmente é um dos principais jogadores do Corinthians.",
+        link: "https://pt.wikipedia.org/wiki/Yuri_Alberto_(futebolista)",
+        tags: "alto brasileiro protagonista centroavante camisa 9 destro "
+
+    },
+    {
+        titulo: "Hugo Souza",
+        descricao: "Hugo Souza é um goleiro brasileiro conhecido por suas defesas importantes. Revelado pelo Flamengo, atualmente defende as cores do Corinthians.",
+        link: "https://pt.wikipedia.org/wiki/Hugo_Souza_(futebolista)",
+        tags: "alto brasileiro goleirao goleirão paredao paredão pegador de pênaltis pegador de penaltis "
+    },
+];
+
+
